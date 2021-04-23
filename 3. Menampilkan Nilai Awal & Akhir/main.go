a2a532f6f85143343baa8e11d8391afb6fd998bb
milhamsuryapratama <m.ilham@majoo.id>
2021-04-23 21:46:40 +0700

    add slice excercise

---
+++ 3. Menampilkan Nilai Awal & Akhir/main.go
@@ -0,0 +1,24 @@
+package main
+
+// import package fmt dan sort
+// https://golang.org/pkg/fmt | https://golang.org/pkg/sort
+
+func main() {
+	// buatlah sebuah array dengan nama nilai, bertipe data int dan mempunyai panjang 7
+	// lalu isi array nilai tersebut dengan angka
+	// https://tour.golang.org/moretypes/6
+
+	// dapatkan tiga nilai paling awal dari array nilai menggunakan teknik 2 index pada slice dan simpan pada variable dengan nama tiga_nilai_awal
+	// dapatkan tiga nilai paling akhir dari array nilai menggunakan teknik 2 index pada slice dan simpan pada variable dengan nama tiga_nilai_akhir
+	// https://tour.golang.org/moretypes/7 | https://gobyexample.com/slices
+
+	// tambahkan satu nilai lagi pada variable slice tiga_nilai_awal dan tiga_nilai akhir menggunakan fungsi append
+	// https://tour.golang.org/moretypes/15
+
+	// urutkan nilai variable slice tiga_nilai_awal dan tiga_nilai_akhir menggunakan fungsi sort.Ints
+	// https://golang.org/pkg/sort/#Ints
+
+	// tampilkan data dari variable slice tiga_nilai_awal dan tiga_nilai_akhir menggunakan fungsi Println pada package fmt
+
+	// tampilkan kapasitas dari variable slice tiga_nilai_awal dan tiga_nilai_akhir menggunakan fungsi Println pada package fmt
+}
