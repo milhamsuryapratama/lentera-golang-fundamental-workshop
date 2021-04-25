d1104bad51c57accc67ecd3a059f97faad67f118
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-04-25 10:49:03 +0700

    add exercise and solution for [menghitung luas bangun datar]

---
+++ 6. Menghitung Luas Bangun Datar/latihan2/main.go
@@ -0,0 +1,48 @@
+package main
+
+// import package fmt
+// https://golang.org/pkg/fmt/
+
+// buatlah konstanta dengan nama phi dan isi value dengan 3.14
+// https://golang.org/doc/effective_go#constants
+
+// buatlah sebuah struct dengan nama Lingkaran dan memiliki properti JariJari dengan tipe data float32
+// https://tour.golang.org/moretypes/2 | https://gobyexample.com/structs
+
+// buatlah fungsi dengan nama hitungLuasLingkaran
+// fungsi hitungLuasLingkaran memiliki dua parameter yaitu
+// parameter pertama adalah lingkaran1 dengan tipe data struct Lingkaran
+// parameter kedua adalah lingkaran2 dengan tipe data struct Lingkaran
+// fungsi hitungLuasLingkaran mereturn dua data dengan tipe data float32
+func hitungLuasLingkaran() {
+	// buatlah perhitungan luas lingkaran 1 dengan rumus phi * JariJari * JariJari
+	// dan simpan pada variable luasLingkaran1
+
+	// buatlah perhitungan luas lingkaran 2 dengan rumus phi * JariJari * JariJari
+	// dan simpan pada variable luasLingkaran2
+
+	// return variable luasLingkaran1 dan luasLingkaran2
+
+	//https://tour.golang.org/basics/6
+}
+
+func main() {
+	// buatlah variable lingkaran1 dengan tipe data struct Lingkaran
+	// isi nilai JariJari lingkaran1 dengan value 14 (value bisa disesuaikan dengn keinginan)
+	// https://dasarpemrogramangolang.novalagung.com/A-struct.html (point A.24.3)
+
+	// buatlah variable lingkaran2 dengan tipe data struct Lingkaran
+	// isi nilai JariJari lingkaran1 dengan value 24 (value bisa disesuaikan dengn keinginan)
+	// https://dasarpemrogramangolang.novalagung.com/A-struct.html (point A.24.3)
+
+	// panggil fungsi hitungLuasLingkaran dan jadikan variable lingkaran1 dan lingkaran2 sebagai parameter
+	// simpan pada dua buah variable yaitu luasLingkaran1 dan luasLingkaran2
+	// https://tour.golang.org/basics/6
+
+	// tampilkan hasil dari luasLingkaran1 dan luasLingkaran2 menggunakan fungsi Println pada package fmt
+	// 📖 https://golang.org/pkg/fmt/#Println
+
+	// buatlah sebuah percabangan yang memvalidasi
+	// jika luasLingkaran1 lebih besar dari luasLingkaran2, maka tampilkan pesan "Luas lingkaran1 lebih luas daripada lingkaran2"
+	// jika luasLingkaran2 lebih besar dari luasLingkaran1, maka tampilkan pesan "Luas lingkaran2 lebih luas daripada lingkaran1"
+}
