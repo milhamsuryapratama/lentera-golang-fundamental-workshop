04e5a6ff396785720d09f49794b07ad8e6757536
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-05-07 00:05:37 +0700

    add exercise 1.3

---
+++ 1. Membuat Program Sederhana/1.2 Menampilkan Bilangan Ganjil Genap/latihan.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	// buatlah perulangan dari 1 sampai 100
+	// https://tour.golang.org/flowcontrol/1
+	for {
+		// lengkapi percabangan dibawah ini
+		// jika angka perulangan adalah genap
+		// https://tour.golang.org/flowcontrol/7
+		// https://golang.org/pkg/fmt/#Println
+		if true {
+			// maka tampilkan, angka adalah bilangan genap
+		} else {
+			// maka tampilkan, angka adalah bilangan ganjil
+		}
+	}
+}
