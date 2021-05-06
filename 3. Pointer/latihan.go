d7ce0e2b9670e5d612801e1fca347e6deaedd869
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-05-07 00:36:13 +0700

    fixing exercise pointer

--- 3. Pointer/latihan.go
+++ 3. Pointer/latihan.go
@@ -1,1 +1,16 @@
 package main
+
+// buatlah fungi ubah_nama dengan parameter nama bertipe data pointer string
+func s() {
+	// ubah value variable nama  menjadi "ilham" (bisa diisi value lain)
+
+	// tampilkan nama pada layar menggunkan fungsi Println pada package fmt
+}
+
+func main() {
+	// buatlah variable nama bertipe data string dengan value "aka" (bisa diisi value lain)
+
+	// panggil fungsi ubah_nama dan isi paramete fungsi ubah_nama dengan pointer dari variable nama
+
+	// tampilkan nama pada layar menggunkan fungsi Println pada package fmt
+}
