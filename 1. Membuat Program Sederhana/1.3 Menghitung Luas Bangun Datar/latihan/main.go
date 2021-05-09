cc21221b4acdc4103959d6ec4dae024e1d7b9dce
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-05-09 12:06:03 +0700

    fixing exercise 1.3

--- 1. Membuat Program Sederhana/1.3 Menghitung Luas Bangun Datar/latihan/main.go
+++ 1. Membuat Program Sederhana/1.3 Menghitung Luas Bangun Datar/latihan/main.go
@@ -4,23 +4,26 @@ import "fmt"
 
 // 💻 buatlah sebuah fungsi dengan nama hitungLuas
 // 💻 berikan parameter alas dan tinggi dengan tipe data float32
-// 💻 pastikan fungsi hitungLuas mereturn tipe data float 32
+// 💻 pastikan fungsi hitungLuas mereturn tipe data float32
 // 📖 https://tour.golang.org/basics/4
-func example() {
+func hitungLuas(alas, tinggi float32) (float32, string) {
+	// catatan : kode didalam blok function
 	// 💻 buatlah varibale luas dan hitung luas segitiga dengan rumus 0.5 * alas * tinggi
-	// 📖 https://tour.golang.org/basics/8
+	luas := 0.5 * alas * tinggi
 
 	// 💻 return nilai dari variable luas
-	// 📖 https://tour.golang.org/basics/4
+	return luas, "Luas Segitiga"
 }
 
 func main() {
 	// 💻 buatlah variable alas dan tinggi dengan tipe data float32 dan isi value dari kedua variable dengan angka bebas
+	var alas float32 = 7
+	var tinggi float32 = 10
 
 	// 💻 panggil fungsi hitungLuas dan isi parameternya dengan variable alas dan tinggi
 	// 💻 simpan hasil kembalian (return) dari fungsi hitungLuas pada variable hasilLuas
-	// 📖 https://tour.golang.org/basics/4
+	hasilLuas, hasil := hitungLuas(alas, tinggi)
 
 	// tampilkan variable hasilLuas dengan fungsi println
-	fmt.Println()
+	fmt.Println(hasil, hasilLuas)
 }
