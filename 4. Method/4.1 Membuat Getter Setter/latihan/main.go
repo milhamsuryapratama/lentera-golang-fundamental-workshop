36bfb779b9cd7cbd305a93550a3da0f6548aa7f6
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-05-15 11:55:17 +0700

    add exercise 4.1

---
+++ 4. Method/4.1 Membuat Getter Setter/latihan/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Siswa struct {
+	Nama   string
+	Alamat string
+}
+
+// 💻 buatlah method dengan nama setNama
+// 💻 ubah properti nama dari struct Siswa menjadi "ilham"
+// 📖 https://tour.golang.org/methods/1
+
+// 💻 buatlah method dengan nama setNama
+// 💻 ubah properti alamat dari struct Siswa menjad "Probolinggo"
+
+// 💻 buatlah method dengan nama GetNama
+// 💻 return properti nama dari struct Siswa
+
+// 💻 buatlah method dengan nama GetAlamat
+// 💻 return properti alamat dari struct Siswa
+
+func main() {
+	var s Siswa
+
+	// 💻 panggil method setNama dan setAlamat
+
+	// 💻 buat variable nama dan panggil method GetNama
+	// 💻 buat variable alamat dan panggil method GetAlamat
+
+	// 💻 tampilkan variable nama dan alamat menggunakan fungsi Println pada package fmt
+	fmt.Println()
+}
