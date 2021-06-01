2750d2f9177c5ec96d28a0aa4b5d7d40716b5260
milhamsuryapratama <ilhamsurya26@gmail.com>
2021-06-01 14:17:04 +0700

    add explanation of reference of teknis 2 index and append

--- 2. Data Structure/2.2 Menampilkan Nilai Awal/latihan/main.go
+++ 2. Data Structure/2.2 Menampilkan Nilai Awal/latihan/main.go
@@ -8,9 +8,11 @@ func main() {
 	nilai := [7]int{80, 90, 87, 70, 95, 84, 70}
 
 	// 💻 dapatkan tiga nilai paling awal dari array nilai  menggunakan teknik 2 index pada slice dan simpan pada variable dengan nama tiga_nilai_awal
+	// dari referesi dibawah, untuk menggunakan teknik 2 index lihat kode pada value dari inisialisai variable `s`
 	// 📖 https://tour.golang.org/moretypes/7 | https://gobyexample.com/slice
 
 	// 💻 tambahkan satu nilai lagi pada variable tiga_nilai_awal menggunakan fungsi append
+	// dari referensi dibawah, fungsi append dapat dilihat pada `func main()` setelah komentar pada kode
 	// 📖 https://tour.golang.org/moretypes/15
 
 	// 💻 tampilkan data dari variable slice tiga_nilai_awal menggunakan fungsi Println pada package fmt
